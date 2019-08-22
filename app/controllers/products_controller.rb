290ac762bd4dc06644cfa2682110d4eb07a4f44c
Аксёнов Александр Сергеевич <aksenov@oscaryard.local>
2019-08-23 00:16:07 +0300

    first creating of world

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,21 @@
-class BeginningController < ApplicationController
+class ProductsController < ApplicationController
   def new
 	@product = Product.new
   end
 
   def create
+	# @product = Product.new(params)
 	@product = Product.new(product_params)
 	if @product.save
       redirect_to '/'
     else
-      render action: "new"
+      render action: 'new'
     end
   end
 
-  def show
-  end
-
-  def index
-  end
-  
   private
   
   def product_params
-	params.require(:product)
+	params.require(:product).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  # get 'products/new'
+  # get 'products/create'
+  # get 'product/new'
+  # get 'product/create'
+  resource :products, only: [:new, :create] do
+	resources :users
+  end
   get 'beginning/new'
   get 'beginning/create'
   get 'beginning/show'
   get 'beginning/index'
-  get '/' => 'beginning#new'
+  get '/' => 'products#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
