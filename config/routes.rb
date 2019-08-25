e3de87af085e8f3c628a436d7470473c47c466c3
NumberI <x0987654321@ya.ru>
2019-08-26 02:49:46 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :roles
   resource :products, only: [:new, :create, :show] do
   	resources :user, only: [:new, :create, :show]
   	resources :info, only: [:new, :create, :show]
   end
   get '/' => 'products#new'
+  root to: "products#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
