3d44b1e4c0414b0c2cefb21600f89b3ea48c4c02
NumberI <x0987654321@ya.ru>
2019-09-05 23:32:07 +0300

    check sign up

--- spec/features/admin_create_user_spec.rb
+++ spec/features/admin_create_user_spec.rb
@@ -11,4 +11,15 @@ feature "user creation" do
     expect(page).to have_content 'Hello, 123@ya.ru'
   end
   
+  scenario "sign up" do
+    visit 'roles/sign_up'
+    # within(".new_role") do
+      fill_in :role_email, :with => '12345@ya.ru'
+      fill_in :role_password, :with => '123456'
+      fill_in :role_password_confirmation, :with => '123456'
+    # end
+    click_button 'Sign up'
+    expect(page).to have_content 'Hello, 12345@ya.ru'
+  end
+
 end
