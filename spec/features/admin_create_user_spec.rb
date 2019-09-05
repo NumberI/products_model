6f585d2e62b0452054c8d9fd59f5b5b81c4ca57a
NumberI <x0987654321@ya.ru>
2019-09-05 21:57:14 +0300

    test sign in

--- spec/features/admin_create_user_spec.rb
+++ spec/features/admin_create_user_spec.rb
@@ -3,8 +3,12 @@ require "rails_helper"
 feature "user creation" do 
   scenario "allows access to page" do
     visit 'products/user/new'
-
-    expect(page).to have_content 'You need to sign in'
+    within(".new_role") do
+      fill_in 'Email', :with => '123@ya.ru'
+      fill_in 'Password', :with => '123456'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Hello, 123@ya.ru'
   end
   
 end
